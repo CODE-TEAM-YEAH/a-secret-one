95e89f4715d77b6496ae85cd6f279f6774354b58
Liam Duncan <liam.a.duncan@gmail.com>
2015-11-04 20:27:39 -0500

    Update Audio.java

--- Audio.java
+++ Audio.java
@@ -1,1 +1,9 @@
-audio
+public class​Audio extends ​Item{
+  protected​String​artistName;
+  public​String​getInfo(...){...} //Returns sNo, Name, Artist
+  name, etc in a string
+  @Override
+  public int​getPrice(...){ // override
+  …
+  …
+}
