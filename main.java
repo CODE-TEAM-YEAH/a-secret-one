ec94baa33ce904b49bc96543965a3847b04ae3d8
DamianAngelone <d-angelone@hotmail.com>
2015-11-04 21:54:29 -0500

    Update main.java

--- main.java
+++ main.java
@@ -5,22 +5,25 @@ public class HWK4_angelodp {
 	public static void main(String[] args) {
 		Boolean correctInput = false;
 		int ChoiceP1 = 0;
+		int CurrentPage = 0;
 		String username = "";
 		
 		while (correctInput == false){
+			CurrentPage = 1;
 			System.out.println("Choose your option:	{1} Sign in\n                    	{2} Sign up");
 			Scanner in1 = new Scanner(System.in);
 			Scanner in2 = new Scanner(System.in);
 			ChoiceP1 = in1.nextInt();
 		
 			if(ChoiceP1 == 1){
-				UserInterface.redirect
+				//....................
 				correctInput = true;
 				System.out.println("Enter your username: ");
-				username = in1.nextLine();
+				username = in2.nextLine();
 			}
 			else if (ChoiceP1 == 2){
 				//....................
+				CurrentPage = 2;
 				correctInput = true;
 				System.out.println("Choose your username: ");
 				username = in2.nextLine();
