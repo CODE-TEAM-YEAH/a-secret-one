48a450a8b76686c875ab736b2468eff3726df30d
Liam Duncan <liam.a.duncan@gmail.com>
2015-11-04 21:06:04 -0500

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -3,8 +3,10 @@ public class UserInterface{
   private array audioProducts;
   private int currentPage; // the page number (P1..P10)
   
-  public int getCurrentPage(...){// This method is for page navigation. Based on the values of the state variable, call different pages
-  public int changeCurrentPage(...){// This method is for page navigation. It should change to current page and show the content.
+  public int getCurrentPage(page){// This method is for page navigation. Based on the values of the state variable, call different pages
+    currentPage = page
+  }
+  public int changeCurrentPage(page){// This method is for page navigation. It should change to current page and show the content.
   public void getReadables(); // Fetches all readables from the files and places them in the readables array
   public void getAudioProducts(); // Fetches all audio products from the files and places them in the readables array
   public void showReadables(); // Displays all readables for browsing
