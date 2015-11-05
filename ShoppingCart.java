c177b4ede69513a872badafad8be72806a6e5cbe
Liam Duncan <liam.a.duncan@gmail.com>
2015-11-04 20:31:39 -0500

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,1 +1,6 @@
-
+public class ShoppingCart extends User{
+  private content //array of items
+  public String getContent(...){// return the content of the
+  shopping cart
+  public addItem (...){}
+}
